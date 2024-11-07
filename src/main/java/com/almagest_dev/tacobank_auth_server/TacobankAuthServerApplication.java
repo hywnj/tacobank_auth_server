3710e4b493fc047954e227388695475263e9bde6
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-11-07 20:11:16 +0900

    init

---
+++ src/main/java/com/almagest_dev/tacobank_auth_server/TacobankAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.almagest_dev.tacobank_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacobankAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacobankAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almagest_dev/tacobank_auth_server/TacobankAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almagest_dev.tacobank_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacobankAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
