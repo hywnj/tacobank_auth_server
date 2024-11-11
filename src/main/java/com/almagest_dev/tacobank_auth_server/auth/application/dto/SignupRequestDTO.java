7dddc28b3ba0b88136cde8a3dea4c7db07488fd0
hyewon <jhjsjym@gmail.com>
2024-11-11 15:58:12 +0900

    Up DTO Annotation

--- src/main/java/com/almagest_dev/tacobank_auth_server/auth/application/dto/SignupRequestDTO.java
+++ src/main/java/com/almagest_dev/tacobank_auth_server/auth/application/dto/SignupRequestDTO.java
@@ -3,9 +3,9 @@ package com.almagest_dev.tacobank_auth_server.auth.application.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class SignupRequestDTO {
     @NotBlank(message = "이메일은 필수 값입니다.")
     @Email(message = "올바른 이메일 형식으로 입력해주세요.")
