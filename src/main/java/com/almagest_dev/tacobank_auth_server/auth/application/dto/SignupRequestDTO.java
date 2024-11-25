be916a0844f22872be9efbe1d841de6394000a11
hyewon <jhjsjym@gmail.com>
2024-11-25 23:39:07 +0900

    Feature 비밀번호 유효성 검사

--- src/main/java/com/almagest_dev/tacobank_auth_server/auth/application/dto/SignupRequestDTO.java
+++ src/main/java/com/almagest_dev/tacobank_auth_server/auth/application/dto/SignupRequestDTO.java
@@ -3,6 +3,7 @@ package com.almagest_dev.tacobank_auth_server.auth.application.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
@@ -12,12 +13,18 @@ public class SignupRequestDTO {
     private String email;
 
     @NotBlank(message = "이름은 필수 값입니다.")
+    @Size(max = 20, message = "이름은 20자 이하여야 합니다.")
     private String name;
 
+    @NotBlank(message = "생년월일은 필수 값입니다.")
+    @Size(min= 6, max = 8, message = "생년월일은 6자리이어야 합니다.")
+    private String birth;
+
     @NotBlank(message = "비밀번호는 필수 값입니다.")
-    @Min(value = 8, message = "비밀번호는 8자 이상이어야 합니다.")
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     private String password;
 
     @NotBlank(message = "전화번호는 필수 값입니다.")
+    @Size(max = 20, message = "전화번호는 20자 이하여야 합니다.")
     private String tel;
 }
